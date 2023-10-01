020a4ce77a423cb30b911a043d5b917575ce0fa4
Kevan Hollbach <khollbach@gmail.com>
2023-10-01 15:38:32 -0400

    Blinky

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,26 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use microbit as _;
+use microbit::{display::blocking, hal::Timer, Board};
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::rtt_init_print;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    rprintln!("Hello, world!");
-    panic!("Goodbye for now.");
+
+    let board = Board::take().unwrap();
+    let mut display = blocking::Display::new(board.display_pins);
+    let mut timer = Timer::new(board.TIMER0);
+
+    loop {
+        for idx in 0..25 {
+            let i = idx / 5;
+            let j = idx % 5;
+            let mut grid = [[0; 5]; 5];
+            grid[i][j] = 1;
+            let delay_ms = 100;
+            display.show(&mut timer, grid, delay_ms);
+        }
+    }
 }
