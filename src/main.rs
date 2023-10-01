6d544236fba1f1a72bff48a21a00a076552319b7
Kevan Hollbach <khollbach@gmail.com>
2023-10-01 15:29:59 -0400

    Enable panic messages

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@
 
 use cortex_m_rt::entry;
 use microbit as _;
-use panic_halt as _;
+use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     rprintln!("Hello, world!");
-    loop {}
+    panic!("Goodbye for now.");
 }
